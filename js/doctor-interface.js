c2f7429769694d2ca2eabf045dcaaad9db00f515
Kate Byars <katebyars5@gmail.com>
2017-09-17 20:49:24 -0700

    safety push.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,22 +2,19 @@ import { Doctor } from './../js/doctor.js';
 
 $(document).ready(function() {
   $('#search-form').submit(function(e) {
-    // console.log("1");
     e.preventDefault();
     var $results = $('#searchResult');
-
     let issue = $('#issue').val();
-  // console.log("2");
     let requestURL = 'https://api.betterdoctor.com/2016-03-01/doctors?';
     var apiKey = require('./../.env').apiKey;
     let resultsOfLookup = Doctor.doctorsByIssue(requestURL, issue, apiKey);
-
-    $results.html(resultsOfLookup);
-      // console.log(resultsOfLookup);
-    // function displayDoctors(resultsOfLookup) {
-    //   $.each(resultsOfLookup, function(i, rep) {
-    //     $('#searchResult').html('<p>' + resultsOfLookup.rep.first_name);
-    //   });
-    // }
+    console.log(resultsOfLookup);
+    function displayResults (resultsOfLookup) {
+      resultsOfLookup.forEach(function(doctor){
+      $("#searchResult").append('<p> '+ doctor.data.first_name + '</p>');
+      });
+    }
   });
+$("input").val("");
+$("#searchResult").empty();
 });
--- js/doctor.js
+++ js/doctor.js
@@ -7,23 +7,23 @@ export let Doctor = {
       'location' : 'or-portland',
       // 'limit' : 9,
     }, function(data) {
-      // if (data.data && data.data.length > 0) {
-      //   let doctors = [];
-      //
-      //   $.each(data.data, function(i, rep) {
-      //     doctors.push(rep.profile);
-      //     // doctors.push(rep.first_name);
-      //     // doctors.push(rep.middle_name);
-      //     // doctors.push(rep.last_name);
-      //     // doctors.push(rep.title);
-      //     // doctors.push(rep.bio);
-      //   });
-      // return doctors;
-      // console.log(doctors);
-      // }
-      // else {
-      //   return '<p>There are no doctors who cover this issue!</p>';
-      // }
+      if (data.data && data.data.length > 0) {
+        let doctors = [];
+
+        $.each(data.data, function(i, rep) {
+          doctors.push(rep.profile);
+          // doctors.push(rep.first_name);
+          // doctors.push(rep.middle_name);
+          // doctors.push(rep.last_name);
+          // doctors.push(rep.title);
+          // doctors.push(rep.bio);
+        });
+      return doctors;
+      console.log("doctor.js" + doctors);
+      }
+      else {
+        return '<p>There are no doctors who cover this issue!</p>';
+      }
     });
   }
 };
