40a68967892a8e8168d715d59284f51ca5317f20
Kate Byars <katebyars5@gmail.com>
2017-09-17 19:21:30 -0700

    add logic for an each loop for results.

--- js/doctor.js
+++ js/doctor.js
@@ -10,6 +10,22 @@ export let Doctor = {
       // 'limit' : 9,
     }, function(data) {
       console.log(data);
+
+      if (data.data && data.data.length > 0 {
+        let doctors = '<p>Here are your results</p>';
+
+        $.each(data.results, function(i, rep) {
+          doctors += '<p>';
+          doctors += rep.image_url;
+          doctors += rep.first_name + ' ' + rep.middle_name+ ' ' +  rep.last_name+ ' ' +  rep.title;
+          doctors += rep.bio;
+          doctors += '</p>';
+        }
+      });
+    return doctors;
+    else {
+      return null;
+    }
     });
   }
 };
