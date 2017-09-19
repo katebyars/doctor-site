8c325ede47ddb019455924aeaf98f6f0b5966e35
Kate Byars <katebyars5@gmail.com>
2017-09-19 15:23:14 -0700

    add display logic.

--- js/doctor.js
+++ js/doctor.js
@@ -1,7 +1,11 @@
 let apiKey = require('./../.env').apiKey;
 export class Doctor {
 
-  constructor(){
+  constructor(first, last, title, bio){
+    this.first = first;
+    this.last = last;
+    this.title = title;
+    this.bio = bio;
   }
 
   doctorsByIssue (issue, user_key){
@@ -14,15 +18,16 @@ export class Doctor {
         let doctors = [];
 
         $.each(data.data, function(i, rep) {
-          doctors.push(rep.profile.first_name);
-          doctors.push(rep.profile.middle_name);
-          doctors.push(rep.profile.last_name);
-          doctors.push(rep.profile.title);
-          doctors.push(rep.profile.bio);
+          let doctor = new Doctor(rep.profile.first_name, rep.profile.last_name, rep.profile.title, rep.profile.bio);
+          doctors.push(doctor);
+          // doctors.push(rep.profile.middle_name);
+          // doctors.push(rep.profile.last_name);
+          // doctors.push(rep.profile.title);
+          // doctors.push(rep.profile.bio);
         });
         console.log("doctor.js" + doctors);
         doctors.forEach(function(doctor){
-          $("#searchResult").append('<p> '+ doctor + '</p>');
+          $("#searchResult").append('<p> '+ doctor.first + '</p>');
         });
       }
       else {
